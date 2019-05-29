c704c9649acc7d0b4217e56212182ace9e63978e
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-29 02:21:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,9 +27,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array.first 
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  array.last 
+  array[-1]
 end
